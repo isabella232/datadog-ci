6eab18c4cb2032c9b5ad162ec43b3b563e3fbce8
Juan Fernandez <juan.fernandezdealba@datadoghq.com>
2020-10-27 09:29:09 +0100

    [ci-app] Fix tags

--- src/helpers/tags.ts
+++ src/helpers/tags.ts
@@ -2,8 +2,8 @@
 export const PARENT_SPAN_ID = 'x-datadog-parent-id'
 export const TRACE_ID = 'x-datadog-trace-id'
 
-export const CI_ENV_PARENT_SPAN_ID = 'X_DATADOG_TRACE_ID'
-export const CI_ENV_TRACE_ID = 'X_DATADOG_PARENT_ID'
+export const CI_ENV_PARENT_SPAN_ID = 'X_DATADOG_PARENT_ID'
+export const CI_ENV_TRACE_ID = 'X_DATADOG_TRACE_ID'
 
 // Build
 export const CI_PIPELINE_URL = 'ci.pipeline.url'
